c68dd10fb19d00b5d557cf4e9ed2eb15d729cb64
milealbirhanu <mileal.birhanu@bitscollege.edu.et>
2026-06-24 13:20:30 +0300

    Add RegularStudent class extending Student

---
+++ src/RegularStudent.java
@@ -0,0 +1,17 @@
+
+    public RegularStudent(
+            int id,
+            String name,
+            String department) {
+
+        super(id, name, department);
+    }
+
+    @Override
+    public void display() {
+
+        System.out.println("\n--- REGULAR STUDENT ---");
+
+        super.display();
+    }
+}
